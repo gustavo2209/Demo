4a65b237b041097a0a17fd7e3a2987783482a0b1
Gustavo Chimoy Garcia <gustavo_fred@hotmail.com>
2022-07-02 14:44:53 -0500

    Agregar archivos de proyecto.

---
+++ Demo/Models/Baldor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class Baldor
+    {
+        public double Suma(params double[] v)
+        {
+            double sum = 0d;
+
+            for(int i = 0; i < v.Length; i++)
+            {
+                sum += v[i];
+            }
+
+            return sum;
+        }
+
+        public double Resta(double a, double b)
+        {
+            double c = a - b;
+
+            return c;
+        }
+
+        public double Multiplica(double a, double b)
+        {
+            double c = a * b;
+
+            return c;
+        }
+
+        public double Divide(double a, double b)
+        {
+            double c = a / b;
+
+            return c;
+        }
+    }
+}
